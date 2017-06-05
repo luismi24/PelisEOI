2c1f57ac7ed76338ae9502239d9a9211aa50c957
Luismi <luismiguel_ml@hotmail.com>
2017-06-05 13:22:35 +0100

    asdfk

--- js/controllers/pelis.controller.js
+++ js/controllers/pelis.controller.js
@@ -5,31 +5,24 @@
         .module('peliculaseoi')
         .controller('PeliculasController', PeliculasController);
 
-    PeliculasController.$inject = ['$scope', 'PeliculasFactory', 'PeliculasHttp'];
-    function PeliculasController($scope, PeliculasFactory, PeliculasHttp) {
-        $scope.searchPelicula = searchPelicula;
+    PeliculasController.$inject = ['$scope', '$http'];
+    function PeliculasController($scope, $http) {
+      //  $scope.searchPelicula = searchPelicula;
         
-
+console.log('hola q ta');
         activate();
 
         ////////////////
 
         function activate() {
+            
             searchPelicula();
          }
          function searchPelicula() {
             return $http.get("https://api.themoviedb.org/3/discover/movie?api_key=6d902fed9905cee850c1703213b4e631&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
                 .then(function(response){
-                    var pelis = [];
-                    console.log(response);
+                    
                 })
         }
-         /*function busquedaPelis(){
-             var searchPelis = $scope.searchPelis;
-             PeliculasHttp.searchPelicula().then(function(response){
-                 $scope.pelis = response;
-             })
-             
-         }*/
     }
 })();
