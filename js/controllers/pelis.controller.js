22b31fc9edcfe0ba4982a0dd731a303df6c783d2
Luismi <luismiguel_ml@hotmail.com>
2017-06-06 09:37:25 +0100

    llamada a la api lista

--- js/controllers/pelis.controller.js
+++ js/controllers/pelis.controller.js
@@ -7,22 +7,25 @@
 
     PeliculasController.$inject = ['$scope', '$http'];
     function PeliculasController($scope, $http) {
-      //  $scope.searchPelicula = searchPelicula;
-        
-console.log('hola q ta');
+        $scope.movies = [];
+        $scope.img = 'https://image.tmdb.org/t/p/w500';
+
         activate();
 
         ////////////////
 
         function activate() {
             
-            searchPelicula();
          }
-         function searchPelicula() {
-            return $http.get("https://api.themoviedb.org/3/discover/movie?api_key=6d902fed9905cee850c1703213b4e631&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+         function iniciales() {
+            $http.get('https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2017&api_key=6d902fed9905cee850c1703213b4e631')
                 .then(function(response){
-                    
-                })
-        }
+                    console.log(response);
+                    $scope.data = response;
+                    $scope.movies = $scope.movies.concat(response.data.results);
+                    console.log($scope.movies);
+                });
+        } 
+        iniciales();
     }
 })();
